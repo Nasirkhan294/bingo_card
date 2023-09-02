dddc3360386cd777fe301f9f1806c69b8fb3f0b9
Nasir <nasirbhatti.nk@gmail.com>
2023-09-02 04:02:11 -0700

    Get the numbers into bingo card

--- bingo_card.rb
+++ bingo_card.rb
@@ -33,6 +33,8 @@ BINGO
 puts bingo_card
 
 header = ["B", "I", "N", "G", "O"]
+n_col.insert(2, "F")
+
 Prawn::Document.generate("bingo.pdf") do
     define_grid(columns: 5, rows: 6)
     
@@ -42,4 +44,40 @@ Prawn::Document.generate("bingo.pdf") do
             text char, align: :center, valign: :center, size: 50, style: :bold
         end
     end
+
+    b_col.each_with_index do |num, i|
+        grid((i +  1), 0).bounding_box do
+            stroke_bounds
+            text num.to_s, align: :center, valign: :center, size: 50, style: :bold
+        end
+    end
+
+    i_col.each_with_index do |num, i|
+        grid((i +  1), 1).bounding_box do
+            stroke_bounds
+            text num.to_s, align: :center, valign: :center, size: 50, style: :bold
+        end
+    end
+
+    n_col.each_with_index do |num, i|
+        grid((i +  1), 2).bounding_box do
+            stroke_bounds
+            text num.to_s, align: :center, valign: :center, size: 50, style: :bold
+        end
+    end
+
+    g_col.each_with_index do |num, i|
+        grid((i +  1), 3).bounding_box do
+            stroke_bounds
+            text num.to_s, align: :center, valign: :center, size: 50, style: :bold
+        end
+    end
+
+    o_col.each_with_index do |num, i|
+        grid((i +  1), 4).bounding_box do
+            stroke_bounds
+            text num.to_s, align: :center, valign: :center, size: 50, style: :bold
+        end
+    end
+    
 end
